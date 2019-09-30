ce5a00e4766b49b0971a39d8029ba14259d9d446
dani5ml <dani2solares@gmail.com>
2019-09-30 17:28:08 +0200

    .php

---
+++ asir2.php
@@ -0,0 +1,17 @@
+
+<?php
+if(isset($_GET['email']))
+$f=fopen('suscriptores.txt','a');
+fwrite($f,$_GET['email']."\r\n");
+fclose($f);
+?>
+<?php
+$f=fopen('visitas.txt','a');
+fwrite($f,$_SERVER['REMOTE_ADDR']."\r\n");
+fclose($f);
+?>
+
+<form>
+<input name="email" type="email"/>
+<button>Suscribirse</button>
+</form>
