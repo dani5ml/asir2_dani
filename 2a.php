4b28c4b5069f62a95bb502994aa2f6cf0a55e309
dani5ml <dani2solares@gmail.com>
2019-10-14 16:14:36 +0200

    2a.php

---
+++ 2a.php
@@ -0,0 +1,10 @@
+<?php
+$a=55;
+$b=40;
+echo "primer numero ".$a.'<br/>';
+echo " segundo numero ".$b.'<br/>';
+echo " resta ".( $a - $b ).'<br/>';
+echo " suma ".( $a + $b ).'<br/>';
+echo " division ".( $a / $b ).'<br/>';
+echo "multiplicacion ".($a * $b). '<br/>';
+?>
